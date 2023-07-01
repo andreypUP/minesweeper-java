62586d6a5c00a3406933425d48000b171cd41052
Seuketchup <123664244+Seuketchup@users.noreply.github.com>
2023-07-01 15:19:00 +0800

    Set up Icon Image

--- src/game/Minesweeper.java
+++ src/game/Minesweeper.java
@@ -1,19 +1,22 @@
 package game;
 
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
 
 public class Minesweeper extends JFrame {
     private JLabel status;
 
     public Minesweeper() {
+
+        ImageIcon icon = new ImageIcon("src//resources//9.png");
+
         status = new JLabel("");
         add(status, BorderLayout.SOUTH);
         add(new Board(status));
         setResizable(false);
         pack();
         setTitle("Minesweeper");
+        setIconImage(icon.getImage());
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
